fcc053f151774e935a1589da304a90d33252977f
dan-mannion <danjohnmannion@gmail.com>
2018-12-09 13:35:12 +0000

    Adds finction wrappping ct settings in fingle function.

---
+++ defineCTSettings.m
@@ -0,0 +1,7 @@
+function defineCTSettings(k, bias, ts, N)
+fprintf(k, 'W0,1E-3,1E-3,%.3EX', ts);
+cmd = sprintf('V%f,0,0,0,%dX',bias, N);
+fprintf(k, cmd);
+fprintf(k, 'G0X');
+fprintf(k, 'O0X');
+end
