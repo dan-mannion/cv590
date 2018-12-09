e1c11b3024f038831ce8c5238849af2f50345cbd
dan-mannion <danjohnmannion@gmail.com>
2018-12-09 16:30:32 +0000

    Corrects cmd variable

--- defineCTSettings.m
+++ defineCTSettings.m
@@ -1,6 +1,6 @@
 function defineCTSettings(k, bias, ts, N)
 fprintf(k, 'W0,1E-3,1E-3,%.3EX', ts);
-cmd = sprintf('V%f,0,0,0,%dX',bias, N);
+cmd = sprintf('V%.3f,0,0,0,%dX',bias, N);
 fprintf(k, cmd);
 fprintf(k, 'G0X');
 fprintf(k, 'O0X');
