45862d10b0883c76f1a696e277eb42e21b105ef1
dan-mannion <danjohnmannion@gmail.com>
2018-12-09 16:31:44 +0000

    Adjusts range

--- Demo_Ct.m
+++ Demo_Ct.m
@@ -1,11 +1,12 @@
 %Setup
 k = connect();
+fprintf(k,'Y2X');
 setFrequency(k, 0);
-setRange(k, 0);
-setReadingRate(k, 1);
+setRange(k, 4);
+setReadingRate(k, 2);
 setFilter(k, 0);
 %Define sweep parameters
-bias = 0.5;
+bias = 2;
 ts = 1;
 num_samples = 5;
 defineCTSettings(k, bias, ts, num_samples);
