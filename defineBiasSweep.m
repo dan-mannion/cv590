d953cc61725d49558ed356604ab2e30433a91ff3
dan-mannion <danjohnmannion@gmail.com>
2019-02-28 11:42:33 +0000

    Change function to sprintf floats not exponents

--- defineBiasSweep.m
+++ defineBiasSweep.m
@@ -1,3 +1,5 @@
 function defineBiasSweep(k, vstart, vend, vinc)
-fprintf(k,sprintf('V%.2E,%.2E,%.2E,0X', vstart, vend, vinc));
+fprintf(k,sprintf('V%.2f,%.2f,%.2f,0X', vstart, vend, vinc));
+% fprintf(k,sprintf('V5,-5,-0.05,0X'));
+
 end
