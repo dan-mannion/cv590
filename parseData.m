c16973b7d83bb62d062d602b288f7a7590f270d0
dan-mannion <danjohnmannion@gmail.com>
2019-02-28 11:43:04 +0000

    Ignores flag indicating frequency.

--- parseData.m
+++ parseData.m
@@ -1,5 +1,6 @@
 function output = parseData(raw_data)
-data = textscan(raw_data,'NTPK%f,%f,%f');
+data = textscan(raw_data,'NTP%*c%f,%f,%f');
+
 output.capacitance = data{1};
 output.conductance =  data{2};
 output.voltage = data{3};
